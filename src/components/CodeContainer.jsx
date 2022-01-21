a26334115e711eb5d4aecbd580362ae1faa3a7b7
T0w3r <torres.alejo2001@gmail.com>
2022-01-21 11:47:03 -0300

    added thunk middleware

--- src/components/CodeContainer.jsx
+++ src/components/CodeContainer.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
-const CodeContainer = ({title,logo,language}) => {
-    const state = useSelector(state => state.code[language]);
-    const [code, setCode] = useState("");
-    const handleChange = (e) => {
-        setCode(e.target.value);
-    }
-  return <div>
-    <h1>{title}</h1>
-    <textarea value={code}  onChange={handleChange} ></textarea>
-  </div>;
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
+const CodeContainer = ({ title, logo, language }) => {
+  const code = useSelector((state) => state.code[language]);
+  const dispatch = useDispatch();
+  const { setCode } = bindActionCreators(actionCreators, dispatch);
+  const handleChange = (e) => {
+    setCode(e.target.value, language);
+  };
+  return (
+    <div>
+      <h1>{title}</h1>
+      <textarea value={code} onChange={handleChange}></textarea>
+    </div>
+  );
 };
 
 export default CodeContainer;
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,8 @@
+export const setCode = (code,language) =>{
+    return (dispatch) =>{
+        dispatch({
+            type: "UPDATE_"+language.toUpperCase(),
+            payload: code
+        })
+    }
+}
