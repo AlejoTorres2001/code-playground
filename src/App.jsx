aef134703911ee5de7e751d8e6d9e0e6123fbf1a
T0w3r <torres.alejo2001@gmail.com>
2022-01-21 11:53:12 -0300

    html +css +js is rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
      <CodeContainer title="HTML" language="html"></CodeContainer>
      <CodeContainer title="CSS" language="css"></CodeContainer>
      <CodeContainer title="JS" language="js"></CodeContainer>
-     <Display html=""></Display>
+     <Display></Display>
     </div>
   )
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
-const Display = ({html,css,js}) => {
+import { useSelector } from 'react-redux';
+const Display = () => {
+  const state = useSelector((state) => state);
+  const makeHtml = () => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <style>
+    ${state.code.css}
+    </style>
+    <head/>
+    <body>
+    <script>
+    ${state.code.js}
+    </script>
+    ${state.code.html}
+    </body>
+    </html>
+    `
+  }
   return <div>
     <h1>Result</h1>
-      <iframe srcDoc={`${html}`}></iframe>
+      <iframe srcDoc={makeHtml()}></iframe>
   </div>;
 };
 
