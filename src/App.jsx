8042921a0bfcfcb92d5533b047de2ea07f906982
T0w3r <torres.alejo2001@gmail.com>
2022-01-24 16:46:05 -0300

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <div className="scrollbar-hide">
     <div className="flex">
       <NavBar/>
     <Split
       minSize={100}
       render={({ getGridProps, getGutterProps }) => (
-        <div className="grid-container overflow-hidden  h-screen w-screen ml-[5rem] bottom-0 " {...getGridProps()}>
+        <div className="grid-container overflow-hidden  h-screen w-screen ml-[4rem] bottom-0 " {...getGridProps()}>
           
           <CodeContainer language="html"></CodeContainer>
           <CodeContainer language="css"></CodeContainer>
@@ -20,12 +20,12 @@ function App() {
           <Display></Display>
           <div />
           <div
-            className="gutter-col gutter-col-1 bg-gray-400"
+            className="gutter-col gutter-col-1 bg-[#2D323C]"
             {...getGutterProps("column", 1)}
           />
           <div />
           <div
-            className="gutter-row gutter-row-1 bg-gray-400"
+            className="gutter-row gutter-row-1 bg-[#2D323C]"
             {...getGutterProps("row", 1)}
           />
           <div />
@@ -33,7 +33,7 @@ function App() {
       )}
     />
   </div>
-  <footer className="flex grow items-center justify-center bg-red-300 mt-0">Alejo Torres</footer>
+  <footer className="flex grow items-center justify-center bg-[#2D323C] mt-0">Alejo Torres</footer>
   </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,52 @@
+import {
+  HomeIcon,
+  AdjustmentsIcon,
+  CollectionIcon,
+  LogoutIcon,
+  SaveAsIcon,
+} from "@heroicons/react/outline";
 const NavBar = () => {
-  return <nav className='w-[5rem] h-screen fixed bg-gray-600'>
-      <ul>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
+  return (
+    <nav className="group z-10 w-[4rem] h-screen fixed bg-[#2D323C] hover:w-64 transition: duration-200 ease-in">
+      <ul className="list-none p-0 m-0 flex flex-col items-center h-full">
+        <li className="w-full">
+          <a className="flex items-center h-[5rem] " href="">
+            <HomeIcon color="#4F525B" width={"5rem"} height={"3rem"} className="m-4 btn-1" ></HomeIcon>
+             <span className="hidden ml-1 group-hover:block 
+             transition: delay-200 duration-200 ease-in-out text-[#C8C8C9]">Playground</span>
+          </a>
+        </li>
+        <li className="w-full">
+          <a className="flex items-center " href="">
+            <CollectionIcon color="#4F525B" className="m-4" width={"5rem"} height={"3rem"}></CollectionIcon>
+            <span className="hidden ml-1 group-hover:block text-[#C8C8C9]">My Playgrounds</span>
+
+          </a>
+        </li>
+        <li className="w-full">
+          <a className="flex items-center " href="">
+            <SaveAsIcon color="#4F525B" className="m-4" width={"5rem"} height={"3rem"}></SaveAsIcon>
+            <span className="hidden ml-1 group-hover:block text-[#C8C8C9]">Save Playground</span>
+
+          </a>
+        </li>
+        <li className="w-full">
+          <a className="flex items-center" href="">
+            <AdjustmentsIcon color="#4F525B" className="m-4" width={"5rem"} height={"3rem"}></AdjustmentsIcon>
+            <span className="hidden ml-1 group-hover:block text-[#C8C8C9]">Settings</span>
+
+          </a>
+        </li>
+        <li className="w-full mt-auto">
+          <a className="flex items-center" href="">
+            <LogoutIcon color="#4F525B " className="m-4" width={"5rem"} height={"3rem"}></LogoutIcon>
+            <span className="hidden ml-1 group-hover:block text-[#C8C8C9]">Log-Out</span>
+
+          </a>
+        </li>
       </ul>
-  </nav>;
+    </nav>
+  );
 };
 
 export default NavBar;
