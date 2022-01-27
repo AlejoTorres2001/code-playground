25071dbe773d7416f08f1de7ac75522e2a272b9c
T0w3r <torres.alejo2001@gmail.com>
2022-01-27 20:04:00 -0300

    suggesting previous name on new Save

--- src/components/PickName.jsx
+++ src/components/PickName.jsx
@@ -1,12 +1,26 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useRef } from "react";
+import { addDoc, collection,doc,updateDoc,getDoc } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
 import { useSelector } from "react-redux";
 import {auth,db} from "../firebase";
+import {useNavigate,useParams} from "react-router-dom"
 const PickName = ({closeModal}) => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const [name, setName] = useState("");
   const state = useSelector((state) => state);
   const [user, loading, error] = useAuthState(auth);
-  const input = useRef();
+  useEffect(() => {
+    if(params?.id){
+      const playground = getDoc(doc(db,"playgrounds",params.id));
+      playground.then(doc=>{
+        setName(doc?.data()?.name)
+      })
+    }
+  }, []);
+  
+  
   const isThereCode = () => {
     return (
       state.code.html !== "" ||
@@ -15,8 +29,8 @@ const PickName = ({closeModal}) => {
     );
   };
   const savePlayground = () => {
-    const name = input.current.value;
-    if (!name) {
+    
+    if (!name || name ==="loading...") {
       console.log("Pick a name!");
       return;
     }
@@ -24,7 +38,22 @@ const PickName = ({closeModal}) => {
       console.log("Add some code!");
       return;
     }
-
+    if(params?.id){
+      //update doc
+      try{
+        updateDoc(doc(db, `playgrounds`, params.id),{
+          "name":name,
+          "code.html":state.code.html,
+          "code.css":state.code.css,
+          "code.javascript":state.code.javascript,
+        })
+      }
+      catch(e){
+        console.log(e)
+      }
+      
+  }
+  else{
     const playground = {
       owner: user.email,
       name: name,
@@ -35,10 +64,10 @@ const PickName = ({closeModal}) => {
       },
     };
     addDoc(collection(db, "playgrounds"), playground).then((docRef) => {
-      console.log(docRef);
-      closeModal();
-
+      navigate(`/${docRef.id}`);
     });
+  }
+  closeModal();
   };
   return (
     <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 z-50">
@@ -46,7 +75,8 @@ const PickName = ({closeModal}) => {
         <h1 class="text-xl mb-4 font-bold text-slate-500">Choose a name</h1>
         <div className="flex flex-col m-2 mb-4">
           <input
-            ref={input}
+            value={name || "loading..."}
+            onChange={e=>setName(e.target.value)}    
             class="border h-5 border-gray-400 appearance-none rounded px-3 py-3  focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
             type="text"
             autofocus
