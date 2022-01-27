74b0eef7754387ede51a96f8a37583c7d9471d9b
T0w3r <torres.alejo2001@gmail.com>
2022-01-27 20:51:21 -0300

    added vars for production

--- src/firebase.js
+++ src/firebase.js
@@ -2,22 +2,37 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider,getAuth, signInWithPopup,signOut} from "firebase/auth";
 import {addDoc,collection,getDocs,getFirestore,query, where} from "firebase/firestore";
-import KEYS from "./keys"
+//import KEYS from "./keys" //uncomment for dev 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: KEYS.API_KEY,
-  authDomain: KEYS.AUTH_DOMAIN,
-  projectId: KEYS.PROJECT_ID,
-  storageBucket: KEYS.STORAGE_BUCKET,
-  messagingSenderId: KEYS.MESSAGING_SENDER_ID,
-  appId: KEYS.APP_ID,
-};
+const setConfig = () =>{
+  if(import.meta.env.MODE === "development"){
+    return  {
+      apiKey: KEYS.API_KEY,
+      authDomain: KEYS.AUTH_DOMAIN,
+      projectId: KEYS.PROJECT_ID,
+      storageBucket: KEYS.STORAGE_BUCKET,
+      messagingSenderId: KEYS.MESSAGING_SENDER_ID,
+      appId: KEYS.APP_ID,
+    };
+  }
+  else{
+    return  {
+      apiKey: import.meta.env.VITE_API_KEY,
+      authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+      projectId: import.meta.env.VITE_PROJECT_ID,
+      storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_APP_ID,
+    };
+  }
+}
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(setConfig());
 const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
