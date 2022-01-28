5c6e4bbfb074ceb812f81b7ca8b8cf988af7fb51
T0w3r <torres.alejo2001@gmail.com>
2022-01-28 16:33:16 -0300

    yuo cannot save playgrounds you dont own

--- src/components/PickName.jsx
+++ src/components/PickName.jsx
@@ -1,26 +1,28 @@
-import { addDoc, collection,doc,updateDoc,getDoc } from "firebase/firestore";
+import { addDoc, collection, doc, updateDoc, getDoc } from "firebase/firestore";
 import React, { useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useDocumentOnce } from "react-firebase-hooks/firestore";
 import { useSelector } from "react-redux";
-import {auth,db} from "../firebase";
-import {useNavigate,useParams} from "react-router-dom"
-const PickName = ({closeModal}) => {
+import { auth, db } from "../firebase";
+import { useNavigate, useParams } from "react-router-dom";
+const PickName = ({ closeModal }) => {
   const navigate = useNavigate();
   const params = useParams();
-  const [name, setName] = useState("");
+  const [playgroundInfo, setPlaygroundInfo] = useState({});
   const state = useSelector((state) => state);
   const [user, loading, error] = useAuthState(auth);
   useEffect(() => {
-    if(params?.id){
-      const playground = getDoc(doc(db,"playgrounds",params.id));
-      playground.then(doc=>{
-        setName(doc?.data()?.name)
-      })
+    if (params?.id) {
+      const playground = getDoc(doc(db, "playgrounds", params.id));
+      playground.then((doc) => {
+        setPlaygroundInfo({
+          name: doc?.data()?.name,
+          owner: doc?.data()?.owner,
+        });
+      });
     }
   }, []);
-  
-  
+
   const isThereCode = () => {
     return (
       state.code.html !== "" ||
@@ -29,8 +31,7 @@ const PickName = ({closeModal}) => {
     );
   };
   const savePlayground = () => {
-    
-    if (!name || name === "") {
+    if (!playgroundInfo.name || playgroundInfo.name === "") {
       console.log("Pick a name!");
       return;
     }
@@ -38,52 +39,59 @@ const PickName = ({closeModal}) => {
       console.log("Add some code!");
       return;
     }
-    if(params?.id){
+    if (playgroundInfo?.owner !== user.email) {
+      console.log("You can't save playgrounds you don't own!");
+      return;
+    }
+    if (params?.id) {
       //update doc
-      try{
-        updateDoc(doc(db, `playgrounds`, params.id),{
-          "name":name,
-          "code.html":state.code.html,
-          "code.css":state.code.css,
-          "code.javascript":state.code.javascript,
-        })
+      try {
+        updateDoc(doc(db, `playgrounds`, params.id), {
+          name: playgroundInfo?.name,
+          "code.html": state.code.html,
+          "code.css": state.code.css,
+          "code.javascript": state.code.javascript,
+        });
+      } catch (e) {
+        console.log(e);
       }
-      catch(e){
-        console.log(e)
-      }
-      
-  }
-  else{
-    //create a new doc
-    const playground = {
-      owner: user.email,
-      name: name,
-      code: {
-        html: state.code.html,
-        css: state.code.css,
-        javascript: state.code.javascript,
-      },
-    };
-    addDoc(collection(db, "playgrounds"), playground).then((docRef) => {
-      navigate(`/${docRef.id}`);
-    });
-  }
-  closeModal();
+    } else {
+      //create a new doc
+      const playground = {
+        owner: user.email,
+        name: playgroundInfo?.name,
+        code: {
+          html: state.code.html,
+          css: state.code.css,
+          javascript: state.code.javascript,
+        },
+      };
+      addDoc(collection(db, "playgrounds"), playground).then((docRef) => {
+        navigate(`/${docRef.id}`);
+      });
+    }
+    closeModal();
   };
+  console.log(playgroundInfo);
   return (
     <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 z-50">
       <div class="bg-white px-16 py-14 rounded-md text-center">
         <h1 class="text-xl mb-4 font-bold text-slate-500">Choose a name</h1>
         <div className="flex flex-col m-2 mb-4">
           <input
-            value={name}
-            onChange={e=>setName(e.target.value)}    
-            class="border h-5 border-gray-400 appearance-none rounded px-3 py-3  focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
+            value={playgroundInfo.name}
+            onChange={(e) =>
+              setPlaygroundInfo({ ...playgroundInfo, name: e.target.value })
+            }
+            className="border h-5 border-gray-400 appearance-none rounded px-3 py-3  focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
             type="text"
             autoFocus
           />
         </div>
-        <button onClick={closeModal} className="bg-indigo-500 px-4 py-2 rounded-md text-md text-white">
+        <button
+          onClick={closeModal}
+          className="bg-indigo-500 px-4 py-2 rounded-md text-md text-white"
+        >
           Cancel
         </button>
         <button
