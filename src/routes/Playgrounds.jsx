4a0481a8071868a2465c13a9ac3997d34fa04049
T0w3r <torres.alejo2001@gmail.com>
2022-01-27 21:46:09 -0300

    quick test

--- src/routes/Playgrounds.jsx
+++ src/routes/Playgrounds.jsx
@@ -7,12 +7,14 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import {db} from "../firebase";
 import { collection, query, where } from "firebase/firestore";
 import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 const Playgrounds = () => {
   const [user, loading, error] = useAuthState(auth);
   const { height, width } = useWindowDimensions();
+  const navigate = useNavigate();
   const q = query(collection(db, "playgrounds"), where("owner", "==", user?.email || ""));
-
   const [value,loadingCollection,errorCollection] = useCollection(q);
+  console.log(value)
    return (
     <div className="scrollbar-hide  bg-red-300">
       <div className="flex">
@@ -24,13 +26,12 @@ const Playgrounds = () => {
         <div className="grow w-screen h-screen ml-[4rem]  flex bg-[#4F525B]">
         {loading && <span>Collection: Loading...</span>}
         {value && (
-          <span>
-            {value.docs.map((doc) => (
-              <p key={doc.id}>
-                {JSON.stringify(doc.data())},{' '}
-              </p>
-            ))}
-          </span>
+         value.docs.map((doc) => (
+              <div className="m-3" key={doc.id}>
+                <h1>{doc.data().name}</h1>
+                <button onClick={()=>{navigate(`/${doc.id}`)}} className="bg-white">go to</button>
+              </div>
+            ))
         )}
         </div>
       </div>
