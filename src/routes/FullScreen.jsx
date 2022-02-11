7197e514807baf049e3f6230038bcb156bee52e2
T0w3r <torres.alejo2001@gmail.com>
2022-02-11 10:58:57 -0300

    formatted

--- src/routes/FullScreen.jsx
+++ src/routes/FullScreen.jsx
@@ -1,33 +1,5 @@
-import { useSelector } from "react-redux";
-
-
+import Display from "../components/Display";
 const FullScreen = () => {
-    const state = useSelector((state) => state);
-    const makeHtml = () => {
-        return `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="UTF-8" />
-        <style>
-        ${state.code.css}
-        </style>
-        <head/>
-        <body>
-        <script>
-        ${state.code.javascript}
-        </script>
-        ${state.code.html}
-        </body>
-        </html>
-        `;
-      };
-      console.log(state);
-  return (
-    <div className="flex">
-        <iframe className="outline-none  grow" srcDoc={makeHtml()}></iframe>
-    </div>
-  )
-
-  }
+  return <Display></Display>;
+};
 export default FullScreen;
