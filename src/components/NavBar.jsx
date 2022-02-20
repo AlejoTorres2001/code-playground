0f2efa6df82ae9dae9d5380156692d9964941165
T0w3r <torres.alejo2001@gmail.com>
2022-02-20 20:11:28 -0300

    added toast when trying to save and not logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,14 +11,18 @@ import { Link } from "react-router-dom";
 import { logOut, signInWithGoogle } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
-import { useLocation,useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
+import { toast, ToastContainer } from "react-toastify";
 const NavBar = ({ openModal }) => {
+  //react-router
   const location = useLocation();
   const navigate = useNavigate();
+  //firabase auth
   const [user, loading, error] = useAuthState(auth);
+  //state-redux
   const code = useSelector((state) => state.code);
   const dispatch = useDispatch();
   const { setCode } = bindActionCreators(actionCreators, dispatch);
@@ -27,20 +31,21 @@ const NavBar = ({ openModal }) => {
     setCode("", "css");
     setCode("", "javascript");
     navigate("/");
-  }
+  };
   const openPickNameModal = () => {
     if (!user) {
+      toast.warning("Sign in to save a playground");
       return;
     }
     openModal();
   };
   const openFullScreen = () => {
-    //open ifreame in new window
+    //open iframe in new window
     navigate(`/fullscreen`);
-
-  }
+  };
   return (
     <nav className="group z-10 w-[4rem] h-screen fixed bg-[#2D323C] hover:w-64 transition: duration-200 ease-in top-0">
+      <ToastContainer></ToastContainer>
       <ul className="list-none p-0 m-0 flex flex-col items-center h-full">
         <li>
           <Link
@@ -56,8 +61,11 @@ const NavBar = ({ openModal }) => {
             <span className="text-[#0C73B8]">y</span>
           </Link>
         </li>
-        <li onClick={goToNewPlayground} className="w-full hover:bg-[#1E1E1E] transition: duration-200 ease-in ">
-          <span  className="flex items-center h-[5rem] " href="blank">
+        <li
+          onClick={goToNewPlayground}
+          className="w-full hover:bg-[#1E1E1E] transition: duration-200 ease-in "
+        >
+          <span className="flex items-center h-[5rem] " href="blank">
             <HomeIcon
               color="#4F525B"
               width={"5rem"}
@@ -65,16 +73,18 @@ const NavBar = ({ openModal }) => {
               className="m-4"
             ></HomeIcon>
             <span
-            href="#"
-            
+              href="#"
               className="hidden ml-1 group-hover:block text-[#C8C8C9] "
             >
               New Playground
             </span>
           </span>
         </li>
-        <li className="w-full hover:bg-[#1E1E1E] transition: duration-200 ease-in ">
-          <Link to="/fullscreen" className="flex items-center h-[5rem] " href="">
+        <li
+          onClick={openFullScreen}
+          className="w-full hover:bg-[#1E1E1E] transition: duration-200 ease-in "
+        >
+          <Link to="#" className="flex items-center h-[5rem] " href="">
             <ArrowsExpandIcon
               color="#4F525B"
               width={"5rem"}
@@ -89,7 +99,7 @@ const NavBar = ({ openModal }) => {
             </Link>
           </Link>
         </li>
-      {/* <button onClick={openFullScreen}>hola</button> */}
+        {/* <button onClick={openFullScreen}>hola</button> */}
         <li className="w-full hover:bg-[#1E1E1E] transition: duration-200 ease-in">
           <Link to="/playgrounds" className="flex items-center " href="">
             <CollectionIcon
