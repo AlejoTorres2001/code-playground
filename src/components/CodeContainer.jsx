e1d6370df1d0de0785a8b79e635dc1043776605d
T0w3r <torres.alejo2001@gmail.com>
2022-02-24 13:30:56 -0300

    configuring eslint

--- src/components/CodeContainer.jsx
+++ src/components/CodeContainer.jsx
@@ -1,27 +1,27 @@
-import { useSelector, useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators } from "../state/index";
-import Editor from "@monaco-editor/react";
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../state/index'
+import Editor from '@monaco-editor/react'
 
 const CodeContainer = ({ language }) => {
-  const code = useSelector((state) => state.code[language]);
-  const dispatch = useDispatch();
-  const { setCode } = bindActionCreators(actionCreators, dispatch);
+  const code = useSelector((state) => state.code[language])
+  const dispatch = useDispatch()
+  const { setCode } = bindActionCreators(actionCreators, dispatch)
   const handleChange = (value) => {
-    setCode(value, language);
-  };
+    setCode(value, language)
+  }
   return (
-    <div className={`flex flex-col focus:outline-none bg-gray-600`}>
+    <div className={'flex flex-col focus:outline-none bg-gray-600'}>
       <Editor
         theme="vs-dark"
-        options={{ fontSize: "18px", automaticLayout: true, wordWrap: "on" }}
+        options={{ fontSize: '18px', automaticLayout: true, wordWrap: 'on' }}
         defaultLanguage={language}
         value={code}
         onChange={handleChange}
         className={`${language}`}
       ></Editor>
     </div>
-  );
-};
+  )
+}
 
-export default CodeContainer;
+export default CodeContainer
