e848722e5b300a23549bdd5060f4fc3afb8bb87c
T0w3r <torres.alejo2001@gmail.com>
2022-02-25 17:12:32 -0300

    caching assets

--- sw.js
+++ sw.js
@@ -1,12 +1,33 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/src/App.jsx",
+  "/src/main.jsx",
+  "/src/components/CodeContainer.jsx",
+  "/src/components/Display.jsx",
+  "/src/components/NavBar.jsx",
+  "/src/components/MobileNavBar.jsx",
+  "/src/components/Footer.jsx",
+  "/src/components/PrivateRoute.jsx",
+  "/src/index.css",
+];
 //install service worker
-self.addEventListener('install', event => {
-    console.log("SW installed")
-})
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
+
+  //console.log("SW installed")
+});
 //activate event
-self.addEventListener('activate', event => {
-    console.log("SW activated")
-})
+self.addEventListener("activate", (event) => {
+  //console.log("SW activated")
+});
 //fecth event
-self.addEventListener('fetch', event => {
-    console.log("SW fetching",event)
-})
+self.addEventListener("fetch", (event) => {
+  //console.log("SW fetching",event)
+});
